38f767ff671ae451a0e60969ceb33502aafc136c
Juan Pico <japm1420@gmail.com>
2023-02-27 13:52:02 -0500

    Directory template 1.0.2

--- src/main/java/com/endava/pokemonChallengue/PokemonChallengueApplication.java
+++ src/main/java/com/endava/pokemonChallengue/PokemonChallengueApplication.java
@@ -2,6 +2,8 @@ package com.endava.pokemonChallengue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PokemonChallengueApplication {
