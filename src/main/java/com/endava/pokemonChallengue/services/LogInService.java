0937b5197c83b4c48b57b81a1efcb7d44f23f635
Juan Andrés Pico <61892156+Jandres1420@users.noreply.github.com>
2023-03-04 15:48:01 -0500

    User class missing

--- src/main/java/com/endava/pokemonChallengue/services/LogInService.java
+++ src/main/java/com/endava/pokemonChallengue/services/LogInService.java
@@ -12,9 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Optional;
 
 @Service
